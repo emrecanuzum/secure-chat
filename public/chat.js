6c1879c8af36262c0f410cb158d15a7ef3d10d34
emrecanuzum <uzumemrecan@gmail.com>
2022-03-14 18:20:41 +0300

    heroku-sadge

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect('https://secure-chat-neon.vercel.app/')
 
 const sender = document.getElementById('sender')
 const message = document.getElementById('message')
