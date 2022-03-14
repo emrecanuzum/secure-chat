672ff1d554a6fd28cc0f485b4cb257c401ccfdf4
emrecanuzum <uzumemrecan@gmail.com>
2022-03-14 18:29:56 +0300

    port 3000

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:5000')
+const socket = io.connect('http://localhost:3000')
 
 const sender = document.getElementById('sender')
 const message = document.getElementById('message')
