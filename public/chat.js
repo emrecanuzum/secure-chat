348c0d687373431f30ef8e7c2af846bca35b3560
AhmetK07 <ahmet.kaan07@hotmail.com>
2022-03-17 22:32:29 +0300

    Fonksiyonel Ahmet

--- public/chat.js
+++ public/chat.js
@@ -5,22 +5,43 @@ const message = document.getElementById('message')
 const submitBtn = document.getElementById('submitBtn')
 const output = document.getElementById('output')
 const feedback = document.getElementById('feedback')
-var SenderName;
 
-Registerbtn.addEventListener('click', () => {
-    SenderName = document.getElementById('sender')
-    })
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
+const name = prompt('what is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
 
-submitBtn.addEventListener('click', () => {
-socket.emit('chat', {
-    message: message.value,
-    sender: SenderName
- })
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
 })
 
-console.log(message.value);
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
 
 socket.on('chat', data => {
+    console.log(data)
     output.innerHTML +='<p><strong>' + data.sender + ' : </strong>' + data.message + '</p>'
 })
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
--- server.js
+++ server.js
@@ -8,8 +8,28 @@ app.use(express.static('public'))
 
 const io = socket(server)
 
+const users = {}
+
 io.on('connection', (socket) =>{
     console.log(socket.id)
+
+    console.log('new User')
+
+    socket.on('new-user', name=> {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+
+    socket.on('send-chat-message', message =>
+    {
+        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
+    })
+
+    socket.on('disconnect', () =>
+    {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
 
     socket.on('chat', data => {
         io.sockets.emit('chat',data)
