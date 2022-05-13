e47e5e4ca9e811df570580c881f99b91cfc363d3
AhmetK07 <ahmet.kaan07@hotmail.com>
2022-05-14 01:40:49 +0300

    RoomsAhmet

--- public/chat.js
+++ public/chat.js
@@ -1,30 +1,40 @@
 const socket = io.connect('http://localhost:3000')
 
-const sender = document.getElementById('sender')
-const message = document.getElementById('message')
-const submitBtn = document.getElementById('submitBtn')
-const output = document.getElementById('output')
-const feedback = document.getElementById('feedback')
-
 const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('what is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+if (messageForm != null) {
+    const name = prompt('what is your name?')
+    appendMessage('You joined')
+    socket.emit('new-user', roomName, name)
+
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const message = messageInput.value
+        //appendMessage(`You: ${message}`)
+        socket.emit('send-chat-message', roomName, message)
+        messageInput.value = ''
+    })
+    
+}
+
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div')
+    roomElement.innerText = room
+    const roomLink = document.createElement('a')
+    roomLink.href = `/${room}`
+    roomLink.innerText = 'join'
+    roomContainer.append(roomElement)
+    roomContainer.append(roomLink)
+})
+
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
 })
 
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message', message)
-    messageInput.value = ''
-})
 
 
 socket.on('user-connected', name => {
